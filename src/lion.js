a638fd2d4483112d0921582152e20787e363625f
Colton Berger <coltonberger@optonline.net>
2018-04-11 13:30:32 -0700

    added solution

--- src/lion.js
+++ src/lion.js
@@ -1,7 +1,8 @@
-var sound = 'ROAR!'
+//var sound = 'ROAR!'
 
 // Only change Line 4
 function lion (sound) {
+  var sound = 'ROAR!'
   return 'The lion goes, "' + sound + '"'
 }
 
--- src/penguin.js
+++ src/penguin.js
@@ -2,10 +2,7 @@
 // to the below code to get the correct statement to return.
 
 var penguin = function (name) {
-  return penguin()
-  var penguin = (function speak () {
     return 'Hello, my name is ' + name
-  })
 }
 
 module.exports = penguin
